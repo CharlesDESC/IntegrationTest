487dbfa1ac739a31d4bf4572d9e7fa73dc15a107
CharlesDESC <charlydant06@gmail.com>
2025-06-24 19:39:15 +0200

    Update home page spec to check for dynamic user count

--- front/cypress/e2e/spec.cy.js
+++ front/cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe("Home page spec", () => {
 	it("deployed react app to localhost", () => {
 		cy.visit("http://localhost:3000");
-		cy.contains("Total users: 0");
+		cy.contains("Total users:");
 	});
 });
